b9656641da116498f0c2b73b42c0a5d4be838acc
Shyam_0373 <shyamtripathi373@gmail.com>
2024-07-30 23:10:47 +0530

    The Code is modified for inputs and Fieldname Printing of tables.

--- utils/Newton_Method.py
+++ utils/Newton_Method.py
@@ -1,17 +1,28 @@
-from sympy import symbols, diff, sympify
+from sympy import symbols, diff, sympify, SympifyError
 from prettytable import PrettyTable
+
+def get_input(prompt, cast_func):
+    while True:
+        try:
+            return cast_func(input(prompt))
+        except ValueError:
+            print("Invalid input. Please try again.")
 
 # Input function, value of x0, symbol, and epsilon
 f_x_input = input("Enter the function (must be quadratic or greater degree, e.g., x**2 + 3*x + 2): ")
-y = float(input("Enter the value of x0: "))
+y = get_input("Enter the value of x0: ", float)
 x_symbol = input("Enter the symbol for the function: ")
-epsilon = float(input("Enter the value of epsilon: "))
+epsilon = get_input("Enter the value of epsilon: ", float)
 
 # Define the symbol
 x = symbols(x_symbol)
 
 # Parse the input function
-f_x = sympify(f_x_input)
+try:
+    f_x = sympify(f_x_input)
+except SympifyError:
+    print("Invalid function. Please enter a valid function.")
+    exit(1)
 
 # Define table
 table = PrettyTable()
@@ -25,16 +36,13 @@ derivative2_f = diff(derivative_f, x)
 last = y + 2 * epsilon  # Ensure the loop starts
 last2 = y
 
-
 def derivative_values(x_value):
     f_prime = derivative_f.evalf(subs={x: x_value})
     f_double_prime = derivative2_f.evalf(subs={x: x_value})
     return f_prime, f_double_prime
 
-
 def f_value(x_value):
     return f_x.evalf(subs={x: x_value})
-
 
 iteration = 1  # Initializing iteration
 
@@ -48,7 +56,7 @@ while abs(last2 - last) > epsilon:
         break
 
     last2 = last2 - (f_prime / f_double_prime)
-    table_row = [iteration, last2, abs(last2 - last)]
+    table_row = [iteration, round(last2, 14), round(abs(last2 - last), 14)]
     table.add_row(table_row)
     iteration += 1
 
@@ -56,5 +64,5 @@ while abs(last2 - last) > epsilon:
 print(table)
 
 # Output the results
-print("Least value (x):", last2)
-print("Optimal Value of function (f(x)):", f_value(last2))
+print("Least value (x):", round(last2, 14))
+print("Optimal Value of function (f(x)):", round(f_value(last2), 14))
