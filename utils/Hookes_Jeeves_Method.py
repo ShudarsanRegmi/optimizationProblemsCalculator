837f5c531472e8cd1d1b0ee731a848958b0b30df
Shyam_0373 <shyamtripathi373@gmail.com>
2024-08-02 11:46:44 +0530

    Now the hookes jeeves method is modified for the dynamic user input of 3 variable based on user input

--- utils/Hookes_Jeeves_Method.py
+++ utils/Hookes_Jeeves_Method.py
@@ -91,3 +91,114 @@ if a == 2:
     fopt = f(X2[0], X2[1])
     print(f'Optimal value of x=[{X2[0]}; {X2[1]}]')
     print(f'Optimal value of f(x)={fopt}')
+
+elif a == 3:
+    x1, x2, x3 = sp.symbols('x1 x2 x3')
+
+    # Define the function
+    f_expr = input("Enter the function containing x1, x2 and x3 variables only: ")
+    f = sp.lambdify((x1, x2, x3), sp.sympify(f_expr), 'numpy')
+
+    # Given values
+    X1 = list(map(int, input("Enter the initial values of x1, x2, x3 separated by space: ").strip().split()))
+    X1 = np.array(X1)
+    delx1 = float(input("Enter the value of delta x1: "))
+    delx2 = float(input("Enter the value of delta x2: "))
+    delx3 = float(input("Enter the value of delta x3: "))
+    ep = float(input("Enter the value of epsilon: "))
+
+    # Hooke-Jeeves search algorithm
+    rsl = []
+    for i in range(20):
+        # Explanatory move in x1 direction
+        A = np.array([X1[0] - delx1, X1[1], X1[2]])
+        B = np.array([X1[0] + delx1, X1[1], X1[2]])
+        fminus = f(A[0], A[1], A[2])
+        ff = f(X1[0], X1[1], X1[2])
+        fplus = f(B[0], B[1], B[2])
+        x_vector = [A, X1, B]
+        f_vector = [fminus, ff, fplus]
+        min_index = np.argmin(f_vector)
+        xmin = x_vector[min_index]
+
+        # Explanatory move in x2 direction
+        A = np.array([xmin[0], xmin[1] - delx2, xmin[2]])
+        B = np.array([xmin[0], xmin[1] + delx2, xmin[2]])
+        fminus = f(A[0], A[1], A[2])
+        ff = f(xmin[0], xmin[1], xmin[2])
+        fplus = f(B[0], B[1], B[2])
+        x_vector = [A, xmin, B]
+        f_vector = [fminus, ff, fplus]
+        min_index = np.argmin(f_vector)
+        xmin = x_vector[min_index]
+
+        # Explanatory move in x3 direction
+        A = np.array([xmin[0], xmin[1], xmin[2] - delx3])
+        B = np.array([xmin[0], xmin[1], xmin[2] + delx3])
+        fminus = f(A[0], A[1], A[2])
+        ff = f(xmin[0], xmin[1], xmin[2])
+        fplus = f(B[0], B[1], B[2])
+        x_vector = [A, xmin, B]
+        f_vector = [fminus, ff, fplus]
+        min_index = np.argmin(f_vector)
+        xmin = x_vector[min_index]
+        A = xmin
+
+        # Pattern search
+        if np.array_equal(X1, A):
+            delx1 /= 2
+            delx2 = delx1 / 2
+            delx3 = delx1 / 2
+            if delx1 <= ep:
+                n = i
+                print(f'The optimal solution is X2 =[{X1[0]};{X1[1]};{X1[2]}]')
+                break
+        else:
+            # Pattern move
+            S = A - X1  # Calculate direction
+
+            # To calculate lambda
+            df_dx1 = sp.diff(f_expr, x1)
+            df_dx2 = sp.diff(f_expr, x2)
+            df_dx3 = sp.diff(f_expr, x3)
+            d2f_dx1x1 = sp.diff(df_dx1, x1)
+            d2f_dx2x2 = sp.diff(df_dx2, x2)
+            d2f_dx3x3 = sp.diff(df_dx3, x3)
+            d2f_dx1x2 = sp.diff(df_dx1, x2)
+            d2f_dx1x3 = sp.diff(df_dx1, x3)
+            d2f_dx2x3 = sp.diff(df_dx2, x3)
+            d2f_dx2x1 = sp.diff(df_dx2, x1)
+            d2f_dx3x1 = sp.diff(df_dx3, x1)
+            d2f_dx3x2 = sp.diff(df_dx3, x2)
+            H = sp.Matrix([[d2f_dx1x1, d2f_dx1x2, d2f_dx1x3],
+                           [d2f_dx2x1, d2f_dx2x2, d2f_dx2x3],
+                           [d2f_dx3x1, d2f_dx3x2, d2f_dx3x3]])
+            delf = sp.Matrix([df_dx1, df_dx2, df_dx3])
+            delfA = delf.subs([(x1, A[0]), (x2, A[1]), (x3, A[2])])
+            S_mat = sp.Matrix(S)
+            lambda_val = -delfA.dot(S_mat) / (S_mat.dot(H * S_mat))
+
+            # New point
+            X2 = np.array(A + lambda_val * S, dtype=float)
+            rsl.append([i, X1.copy(), A.copy(), S, float(lambda_val), X2.copy()])
+
+            # Check optimality
+            delfX2 = delf.subs([(x1, X2[0]), (x2, X2[1]), (x3, X2[2])])
+            if delfX2[0] == 0 and delfX2[1] == 0 and delfX2[2] == 0:
+                n = i
+                print(f'The optimal solution is X2 =[{X2[0]};{X2[1]};{X2[2]}]')
+                break
+            else:
+                X1 = X2
+
+    # Create a result table
+    Variables = ['k', 'Initial value', 'Explanatory move', 'Direction S', 'lambda', 'Pattern move']
+    Resl = pd.DataFrame(rsl, columns=Variables)
+    print(Resl)
+
+    # Optimal value
+    fopt = f(X2[0], X2[1], X2[2])
+    print(f'Optimal value of x=[{X2[0]}; {X2[1]}; {X2[2]}]')
+    print(f'Optimal value of f(x)={fopt}')
+
+
